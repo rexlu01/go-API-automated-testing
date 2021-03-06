5fa48613c971be1da2725a6231ac6130a7dc513e
luping <862660488@qq.com>
2021-03-06 19:40:16 +0800

    update connect_consul_method

--- common/connect_database.go
+++ common/connect_database.go
@@ -1,5 +1,12 @@
 package common
 
+import (
+	"fmt"
+	"go-api-automated-testing/config"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
 type MysqlConfig struct {
 	Name     string
 	Password string
@@ -14,4 +21,33 @@ type RedisConfig struct {
 	RedisAddr  string
 	Password   string
 	DataDase   int
+}
+
+/*--------------------consul相关操作--------------------------*/
+func GetConsulConn(config *config.Localserver) (*consulapi.Client, error) {
+	// 连接consul
+	conn := consulapi.DefaultConfig()
+	conn.Address = config.ConsulAddress
+
+	client, err := consulapi.NewClient(conn)
+	if err != nil {
+		fmt.Println("consul client error: ", err)
+		return nil, err
+	}
+	return client, nil
+
+}
+
+func GetConsulKV(config *config.Localserver, key string) ([]byte, error) {
+	client, err := GetConsulConn(config)
+	if err != nil {
+		panic(err)
+	}
+
+	data, _, err := client.KV().Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	return data.Value, nil
+
 }
