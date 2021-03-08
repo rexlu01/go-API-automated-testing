3c70f0fed5fb74d4e0ad8edb9ca131ffa1d6dfaa
luping <862660488@qq.com>
2021-03-08 20:07:09 +0800

    update go micro

--- data/server/server.go
+++ data/server/server.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"go-api-automated-testing/common"
 	"go-api-automated-testing/config"
+	pb "go-api-automated-testing/data/server/proto"
 )
 
+type StoreData struct {
+}
+
+func (g *StoreData) Store(ctx context.Context, req *pb.StoreRequest, rsp *pb.StoreRespons) error {
+	//这里写拿到处理好的数据往数据库里存
+	testdata := []string{"1.1", "1.2"}
+
+	req.Details = testdata
+	return nil
+}
 func main() {
 
 	localPath := common.GetLocalPath()
