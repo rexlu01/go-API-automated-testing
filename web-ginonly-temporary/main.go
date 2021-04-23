245945d2997951554ebfd9d26c3147136ef78305
rexlu <862660488@qq.com>
2021-04-23 17:53:12 +0800

    update web main

--- web-ginonly-temporary/main.go
+++ web-ginonly-temporary/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go-api-automated-testing/web/mon"
 	"html/template"
 	"log"
@@ -27,18 +26,27 @@ func Image(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	monit.MaxRecord = 50
+	go monit.Run()
 
 	ginRouter := gin.Default()
 	ginRouter.LoadHTMLGlob("html/*")
 	////加载静态资源，例如网页的css、js
 	ginRouter.Static("/static", "./static")
 	ginRouter.GET("/sendmweb/", SendMweb)
-	ginRouter.POST("/form", From)
+
+	ginRouter.POST("/form", func(c *gin.Context) {
+
+		_ = c.PostForm("requestname")
+
+		c.Request.URL.Path = "/monit"
+		c.Request.Method = "GET"
+		ginRouter.HandleContext(c)
+
+	})
 
 	ginRouter.GET("/monit", gin.WrapF(Index))
 	ginRouter.GET("/image", gin.WrapF(Image))
 
-	go monit.Run()
 	ginRouter.Run(":5656")
 }
 
@@ -46,12 +54,4 @@ func SendMweb(c *gin.Context) {
 	log.Print("Received Start.SendMessage API Request")
 
 	c.HTML(200, "index.html", gin.H{})
-}
-
-func From(c *gin.Context) {
-
-	userName := c.PostForm("requestname")
-
-	c.String(http.StatusOK, fmt.Sprintf("username:%s", userName))
-
 }
